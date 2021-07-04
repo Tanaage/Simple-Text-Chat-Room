8b1d150b80aaad547d3dc34e66cc383dd3d0618e
5icksteen <sicksteen@me.com>
2021-07-05 01:25:42 +0900

    wip

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const Peer = window.Peer;
 
+window.__SKYWAY_KEY__ = '7f6811d4-2b08-4bd7-8be8-cd036923e473';
+
 (async function main() {
   const joinTrigger = document.getElementById('js-join-trigger');
   const leaveTrigger = document.getElementById('js-leave-trigger');
   const roomId = document.getElementById('js-room-id');
-  const roomMode = document.getElementById('js-room-mode');
   const localText = document.getElementById('js-local-text');
   const sendTrigger = document.getElementById('js-send-trigger');
   const messages = document.getElementById('js-messages');
+  // document.getElementById('js-user-id').value = '名無し';
+  // let userId = '名無し';
+  const userId = document.getElementById('js-user-id');
 
-  // eslint-disable-next-line require-atomic-updates
-  const peer = (window.peer = new Peer({
-    key: '7f6811d4-2b08-4bd7-8be8-cd036923e473',
-    debug: 3,
-  }));
+  const peer = (window.peer = new Peer({ key: '7f6811d4-2b08-4bd7-8be8-cd036923e473', }));
+
+  // let user_id = '名無し';
 
   // Register join handler
   joinTrigger.addEventListener('click', () => {
@@ -23,42 +25,43 @@ const Peer = window.Peer;
       return;
     }
 
-    const room = peer.joinRoom(roomId.value, {
-      mode: 'mesh',
-      stream: localStream,
-    });
+    userId.disabled = true;
+    let user_name = userId.value;
 
-    room.once('open', () => {
-      messages.textContent += '=== You joined ===\n';
-    });
-    room.on('peerJoin', peerId => {
-      messages.textContent += `=== ${peerId} joined ===\n`;
-    });
+    if (user_name == '') {
+      user_name = '名無し';
+    }
 
-    room.on('data', ({ data, src }) => {
-      // Show a message sent to the room and who sent
-      messages.textContent += `${src}: ${data}\n`;
-    });
+    // let userId = '名無し';
+    // userId = document.getElementById('js-user-id');
+
+    const room = peer.joinRoom(roomId.value, { mode: 'mesh', });
+
+    room.once('open', () => { messages.textContent += '=== You joined ===\n'; });
+    // room.on('peerJoin', peerId => { messages.textContent += `=== ${peerId} joined ===\n`; });
+    room.on('peerJoin', () => { messages.textContent += `=== ${user_name} joined ===\n`; });
+
+    // room.on('data', ({ data, src }) => { messages.textContent += `${src}: ${data}\n`; });// Show a message sent to the room and who sent
 
     // for closing room members
-    room.on('peerLeave', peerId => {
-      messages.textContent += `=== ${peerId} left ===\n`;
-    });
+    // room.on('peerLeave', peerId => { messages.textContent += `=== ${peerId} left ===\n`; });
+    room.on('peerLeave', () => { messages.textContent += `=== ${user_name} left ===\n`; });
 
     // for closing myself
     room.once('close', () => {
       sendTrigger.removeEventListener('click', onClickSend);
-      messages.textContent += '== You left ===\n';
+      messages.textContent += '=== You left ===\n';
     });
 
     sendTrigger.addEventListener('click', onClickSend);
-    leaveTrigger.addEventListener('click', () => room.close(), { once: true });
+    leaveTrigger.addEventListener('click', () => { userId.disabled = false; room.close(), { once: true } });
 
     function onClickSend() {
       // Send message to all of the peers in the room via websocket
       room.send(localText.value);
 
-      messages.textContent += `${peer.id}: ${localText.value}\n`;
+      messages.textContent += `${user_name}: ${localText.value}\n`;
+      // messages.textContent += `${peer.id}: ${localText.value}\n`;
       localText.value = '';
     }
   });
